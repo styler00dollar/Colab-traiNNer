db3e2a147fa09343cdc9c9093165f13345745fe8
styler00dollar <51405565+styler00dollar@users.noreply.github.com>
2021-09-13 13:38:59 +0200

    adding scene detect splitter

---
+++ code/scripts/scenedetect.py
@@ -0,0 +1,14 @@
+import glob
+import os
+
+rootdir = '/atm/'
+files = glob.glob(rootdir + '/**/*.mkv', recursive=True)
+files_webm = glob.glob(rootdir + '/**/*.webm', recursive=True)
+files_mp4 = glob.glob(rootdir + '/**/*.mp4', recursive=True)
+files.extend(files_webm)
+files.extend(files_mp4)
+
+
+for f in files:
+	os.system(f'scenedetect --input "{f}" detect-content -t 1 list-scenes split-video --copy')
+	os.remove(f)
