214559c6445dc179731998b31d19974c88e6e001
styler00dollar <styler00dollar64@gmail.com>
2021-12-26 10:54:07 +0100

    updating rife arch to 4.0

--- code/arch/rife_arch.py
+++ code/arch/rife_arch.py
@@ -1,19 +1,21 @@
 """
-rife 3.8
+26-Dez-21
 https://github.com/hzwer/Practical-RIFE
-
-warplayer.py
+https://drive.google.com/file/d/1mUK9iON6Es14oK46-cCflRoPTeGiI_A9/view
 https://github.com/hzwer/Practical-RIFE/blob/main/model/warplayer.py
+https://github.com/HolyWu/vs-rife/blob/master/vsrife/__init__.py
 """
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.optim import AdamW
+import itertools
+import numpy as np
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
-import torch
 import torch.nn as nn
+import torch.optim as optim
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 backwarp_tenGrid = {}
-
 
 def warp(tenInput, tenFlow):
     k = (str(tenFlow.device), str(tenFlow.size()))
@@ -31,8 +33,6 @@ def warp(tenInput, tenFlow):
     g = (backwarp_tenGrid[k] + tenFlow).permute(0, 2, 3, 1)
     return torch.nn.functional.grid_sample(input=tenInput, grid=g, mode='bilinear', padding_mode='border', align_corners=True)
 
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def conv(in_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1):
     return nn.Sequential(
@@ -56,108 +56,179 @@ class IFBlock(nn.Module):
             conv(in_planes, c//2, 3, 2, 1),
             conv(c//2, c, 3, 2, 1),
             )
-        self.convblock0 = nn.Sequential(
+        self.convblock = nn.Sequential(
             conv(c, c),
-            conv(c, c)
-        )
-        self.convblock1 = nn.Sequential(
             conv(c, c),
-            conv(c, c)
-        )
-        self.convblock2 = nn.Sequential(
             conv(c, c),
-            conv(c, c)
-        )
-        self.convblock3 = nn.Sequential(
             conv(c, c),
-            conv(c, c)
+            conv(c, c),
+            conv(c, c),
+            conv(c, c),
+            conv(c, c),
         )
-        self.conv1 = nn.Sequential(
-            nn.ConvTranspose2d(c, c//2, 4, 2, 1),
-            nn.PReLU(c//2),
-            nn.ConvTranspose2d(c//2, 4, 4, 2, 1),
-        )
-        self.conv2 = nn.Sequential(
-            nn.ConvTranspose2d(c, c//2, 4, 2, 1),
-            nn.PReLU(c//2),
-            nn.ConvTranspose2d(c//2, 1, 4, 2, 1),
-        )
+        self.lastconv = nn.ConvTranspose2d(c, 5, 4, 2, 1)
 
-    def forward(self, x, flow, scale=1):
-        x = F.interpolate(x, scale_factor= 1. / scale, mode="bilinear", align_corners=False, recompute_scale_factor=False)
-        flow = F.interpolate(flow, scale_factor= 1. / scale, mode="bilinear", align_corners=False, recompute_scale_factor=False) * 1. / scale
-        feat = self.conv0(torch.cat((x, flow), 1))
-        feat = self.convblock0(feat) + feat
-        feat = self.convblock1(feat) + feat
-        feat = self.convblock2(feat) + feat
-        feat = self.convblock3(feat) + feat        
-        flow = self.conv1(feat)
-        mask = self.conv2(feat)
-        flow = F.interpolate(flow, scale_factor=scale, mode="bilinear", align_corners=False, recompute_scale_factor=False) * scale
-        mask = F.interpolate(mask, scale_factor=scale, mode="bilinear", align_corners=False, recompute_scale_factor=False)
+    def forward(self, x, flow=None, scale=1):
+        x = F.interpolate(x, scale_factor= 1. / scale, mode="bilinear", align_corners=False)
+        if flow is not None:
+            flow = F.interpolate(flow, scale_factor= 1. / scale, mode="bilinear", align_corners=False) * 1. / scale
+            x = torch.cat((x, flow), 1)
+        feat = self.conv0(x)
+        feat = self.convblock(feat) + feat
+        tmp = self.lastconv(feat)
+        tmp = F.interpolate(tmp, scale_factor=scale*2, mode="bilinear", align_corners=False)
+        flow = tmp[:, :4] * scale * 2
+        mask = tmp[:, 4:5]
         return flow, mask
         
 class IFNet(nn.Module):
     def __init__(self):
         super(IFNet, self).__init__()
-        self.block0 = IFBlock(7+4, c=90)
-        self.block1 = IFBlock(7+4, c=90)
-        self.block2 = IFBlock(7+4, c=90)
-        # self.contextnet = Contextnet()
-        # self.unet = Unet()
+        self.block0 = IFBlock(7, c=192)
+        self.block1 = IFBlock(8+4, c=128)
+        self.block2 = IFBlock(8+4, c=96)
+        self.block3 = IFBlock(8+4, c=64)
+        self.contextnet = Contextnet()
+        self.unet = Unet()
 
-    def forward(self, img0, img1, scale_list=[4, 2, 1], training=False, ada_scale=True, ensemble=False):
-        
+    def forward( self, img0, img1, timestep=0.5, scale_list=[8, 4, 2, 1], training=True, fastmode=True, ensemble=False):
         n, c, h, w = img0.shape
         ph = ((h - 1) // 64 + 1) * 64
         pw = ((w - 1) // 64 + 1) * 64
         padding = (0, pw - w, 0, ph - h)
         img0 = F.pad(img0, padding)
         img1 = F.pad(img1, padding)
-        
-        """
+        x = torch.cat((img0, img1), 1)
+
         if training == False:
             channel = x.shape[1] // 2
             img0 = x[:, :channel]
             img1 = x[:, channel:]
-        """
+        if not torch.is_tensor(timestep):
+            timestep = (x[:, :1].clone() * 0 + 1) * timestep
+        else:
+            timestep = timestep.repeat(1, 1, img0.shape[2], img0.shape[3])
         flow_list = []
         merged = []
         mask_list = []
         warped_img0 = img0
         warped_img1 = img1
-        
-        #x = torch.cat((img0,img0), dim=1)
-        flow = torch.zeros_like(torch.cat((img0,img0), dim=1)[:, :4]).to(device)
-        mask = torch.zeros_like(torch.cat((img0,img0), dim=1)[:, :1]).to(device)
-
-        ada_scale = scale_list.copy()
-        if ada_scale:
-            flow_test, _ = self.block0(torch.cat((img0[:, :3], img1[:, :3], mask), 1), flow, scale=ada_scale[0] * 2)
-            if flow_test.abs().max() > ada_scale[0] * 16:
-                for i in range(3):
-                    ada_scale[i] = ada_scale[i] * 2
+        flow = None
+        mask = None
         loss_cons = 0
-        block = [self.block0, self.block1, self.block2]
-        for i in range(3):
-            f0, m0 = block[i](torch.cat((warped_img0[:, :3], warped_img1[:, :3], mask), 1), flow, scale=ada_scale[i])
-            if ensemble:
-            	f1, m1 = block[i](torch.cat((warped_img1[:, :3], warped_img0[:, :3], -mask), 1), torch.cat((flow[:, 2:4], flow[:, :2]), 1), scale=ada_scale[i])
-            	f0 = (f0 + torch.cat((f1[:, 2:4], f1[:, :2]), 1)) / 2
-            	m0 = (m0 + (-m1)) / 2
-            flow = flow + f0
-            mask = mask + m0
+        block = [self.block0, self.block1, self.block2, self.block3]
+        for i in range(4):
+            if flow is None:
+                flow, mask = block[i](torch.cat((img0[:, :3], img1[:, :3], timestep), 1), None, scale=scale_list[i])
+                if ensemble:
+                    f1, m1 = block[i](torch.cat((img1[:, :3], img0[:, :3], 1-timestep), 1), None, scale=scale_list[i])
+                    flow = (flow + torch.cat((f1[:, 2:4], f1[:, :2]), 1)) / 2
+                    mask = (mask + (-m1)) / 2
+            else:
+                f0, m0 = block[i](torch.cat((warped_img0[:, :3], warped_img1[:, :3], timestep, mask), 1), flow, scale=scale_list[i])
+                if i == 1 and f0[:, :2].abs().max() > 32 and f0[:, 2:4].abs().max() > 32 and not training:
+                    for k in range(4):
+                        scale_list[k] *= 2
+                    flow, mask = block[0](torch.cat((img0[:, :3], img1[:, :3], timestep), 1), None, scale=scale_list[0])
+                    warped_img0 = warp(img0, flow[:, :2])
+                    warped_img1 = warp(img1, flow[:, 2:4])
+                    f0, m0 = block[i](torch.cat((warped_img0[:, :3], warped_img1[:, :3], timestep, mask), 1), flow, scale=scale_list[i])
+                if ensemble:
+            	    f1, m1 = block[i](torch.cat((warped_img1[:, :3], warped_img0[:, :3], 1-timestep, -mask), 1), torch.cat((flow[:, 2:4], flow[:, :2]), 1), scale=scale_list[i])
+            	    f0 = (f0 + torch.cat((f1[:, 2:4], f1[:, :2]), 1)) / 2
+            	    m0 = (m0 + (-m1)) / 2
+                flow = flow + f0
+                mask = mask + m0
             mask_list.append(mask)
             flow_list.append(flow)
             warped_img0 = warp(img0, flow[:, :2])
             warped_img1 = warp(img1, flow[:, 2:4])
             merged.append((warped_img0, warped_img1))
-        '''
-        c0 = self.contextnet(img0, flow[:, :2])
-        c1 = self.contextnet(img1, flow[:, 2:4])
-        tmp = self.unet(img0, img1, warped_img0, warped_img1, mask, flow, c0, c1)
-        res = tmp[:, 1:4] * 2 - 1
-        '''
-        mask_list[2] = torch.sigmoid(mask_list[2])
-        merged[2] = merged[2][0] * mask_list[2] + merged[2][1] * (1 - mask_list[2])
-        return merged[2][:, :, :h , :w]
+        mask_list[3] = torch.sigmoid(mask_list[3])
+        merged[3] = merged[3][0] * mask_list[3] + merged[3][1] * (1 - mask_list[3])
+        if not fastmode:
+            c0 = self.contextnet(img0, flow[:, :2])
+            c1 = self.contextnet(img1, flow[:, 2:4])
+            tmp = self.unet(img0, img1, warped_img0, warped_img1, mask, flow, c0, c1)
+            res = tmp[:, :3] * 2 - 1
+            merged[3] = torch.clamp(merged[3] + res, 0, 1)
+        return merged[3][:, :, :h , :w]
+
+def conv(in_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1):
+    return nn.Sequential(
+        nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
+                  padding=padding, dilation=dilation, bias=True),
+        nn.PReLU(out_planes)
+        )
+
+def conv_woact(in_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1):
+    return nn.Sequential(
+        nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
+                  padding=padding, dilation=dilation, bias=True),
+        )
+
+def deconv(in_planes, out_planes, kernel_size=4, stride=2, padding=1):
+    return nn.Sequential(
+        torch.nn.ConvTranspose2d(in_channels=in_planes, out_channels=out_planes, kernel_size=4, stride=2, padding=1, bias=True),
+        nn.PReLU(out_planes)
+        )
+            
+class Conv2(nn.Module):
+    def __init__(self, in_planes, out_planes, stride=2):
+        super(Conv2, self).__init__()
+        self.conv1 = conv(in_planes, out_planes, 3, stride, 1)
+        self.conv2 = conv(out_planes, out_planes, 3, 1, 1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        return x
+    
+c = 16
+class Contextnet(nn.Module):
+    def __init__(self):
+        super(Contextnet, self).__init__()
+        self.conv1 = Conv2(3, c)
+        self.conv2 = Conv2(c, 2*c)
+        self.conv3 = Conv2(2*c, 4*c)
+        self.conv4 = Conv2(4*c, 8*c)
+    
+    def forward(self, x, flow):
+        x = self.conv1(x)
+        flow = F.interpolate(flow, scale_factor=0.5, mode="bilinear", align_corners=False) * 0.5
+        f1 = warp(x, flow)        
+        x = self.conv2(x)
+        flow = F.interpolate(flow, scale_factor=0.5, mode="bilinear", align_corners=False) * 0.5
+        f2 = warp(x, flow)
+        x = self.conv3(x)
+        flow = F.interpolate(flow, scale_factor=0.5, mode="bilinear", align_corners=False) * 0.5
+        f3 = warp(x, flow)
+        x = self.conv4(x)
+        flow = F.interpolate(flow, scale_factor=0.5, mode="bilinear", align_corners=False) * 0.5
+        f4 = warp(x, flow)
+        return [f1, f2, f3, f4]
+    
+class Unet(nn.Module):
+    def __init__(self):
+        super(Unet, self).__init__()
+        self.down0 = Conv2(17, 2*c)
+        self.down1 = Conv2(4*c, 4*c)
+        self.down2 = Conv2(8*c, 8*c)
+        self.down3 = Conv2(16*c, 16*c)
+        self.up0 = deconv(32*c, 8*c)
+        self.up1 = deconv(16*c, 4*c)
+        self.up2 = deconv(8*c, 2*c)
+        self.up3 = deconv(4*c, c)
+        self.conv = nn.Conv2d(c, 3, 3, 1, 1)
+
+    def forward(self, img0, img1, warped_img0, warped_img1, mask, flow, c0, c1):
+        s0 = self.down0(torch.cat((img0, img1, warped_img0, warped_img1, mask, flow), 1))
+        s1 = self.down1(torch.cat((s0, c0[0], c1[0]), 1))
+        s2 = self.down2(torch.cat((s1, c0[1], c1[1]), 1))
+        s3 = self.down3(torch.cat((s2, c0[2], c1[2]), 1))
+        x = self.up0(torch.cat((s3, c0[3], c1[3]), 1))
+        x = self.up1(torch.cat((x, s2), 1)) 
+        x = self.up2(torch.cat((x, s1), 1)) 
+        x = self.up3(torch.cat((x, s0), 1)) 
+        x = self.conv(x)
+        return torch.sigmoid(x)
