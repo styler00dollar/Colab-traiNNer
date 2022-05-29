031b82c68b29c2172e4c28e8911ccc465aaf0585
styler00dollar <51405565+styler00dollar@users.noreply.github.com>
2022-05-29 20:13:13 +0200

    adding script to extract state dicts from checkpoint files

---
+++ code/scripts/extract_models.py
@@ -0,0 +1,21 @@
+import torch
+path = "70000.ckpt"
+
+checkpoint = torch.load(path)
+
+new_model_G = {}
+new_model_D = {}
+
+for i, j in checkpoint["state_dict"].items():
+  print(i)
+
+  if "netG." in i:
+    key = i.replace("netG.", "")
+    new_model_G[key] = j
+
+  if "netD." in i:
+    key = i.replace("netD.", "")
+    new_model_D[key] = j 
+
+torch.save(new_model_G, "G.pth")
+torch.save(new_model_D, "D.pth")
